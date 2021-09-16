26fbe7d441fbd8e9e0aa0ddbc5bd83274b348a18
JulioBeebit <julio.jawad@beebit.es>
2021-09-16 15:53:49 +0200

    comm1

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryURL: "git@github.com:JulioBeebit/react-app.git",
+  plugins: ['@semantic-release/commit-analyzer',
+            '@semantic-release/release-notes-generator',
+            '@semantic-release/github'
+           ]
+
+}
